da419bd446233d22349da238ee032da1c9dc676f
jomoartz <johnjohananmogbolu@gmail.com>
2025-10-23 02:45:06 +0100

    feat: layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView
+        style={{
+          flex: 1,
+        }}
+      >
+        <Stack />
+      </SafeAreaView>
+    </>
+  );
 }
